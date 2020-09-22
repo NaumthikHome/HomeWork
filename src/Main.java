9552ac72cae73bb27ac5672ef2596d57ac84083d
naumthicartem@gmail.com <@SCH3.LOCAL>
2020-09-22 15:40:08 +0300

    проба

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String a="Привет";
+        System.out.println("переменная типа текст "+a);
+
+        byte c=27;
+        System.out.println("переменная типа byte "+c);
+
+        short d=99;
+        System.out.println("переменная типа short "+d);
+
+        int b;
+        b = 117;
+        System.out.println("переменная типа int "+b);
+
+        long e=1000;
+        System.out.println("переменная типа long "+e);
+
+        double r=45.7;
+        System.out.println("переменная типа double "+r);
+
+        float y=222;
+        System.out.println("переменная типа float "+y);
+
+        char q='8';
+        System.out.println("переменная типа char "+q);
+
+        System.out.println(b+q);
+
+        boolean t;
+        t = true;
+        System.out.println("переменная типа boolean "+t);
+
+        int a1=5;
+        int b1= a1++;
+
+        int a2=10;
+        int b2=++a2;
+
+        int a3=15;
+        int b3=a3--;
+
+        int a4=20;
+        int b4=--a4;
+	// write your code here
+    }
+}
